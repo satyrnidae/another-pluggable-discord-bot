49ab39bcc7ef0fd25b55826aea22ad30fbd215a3
Bridger Maskrey <maskreybe@outlook.com>
2019-11-08 13:10:28 -0500

    🎨 Set locale, lifecycle tweaks

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ i18n.configure({
     logWarnFn: (msg) => console.warn(msg),
     logErrorFn: (msg) => console.error(msg)
 });
+i18n.setLocale('en');
 
 // Set Up Dependencies
 Container.bind<CommandRegistry>(SERVICE_IDENTIFIERS.COMMAND_REGISTRY).to(CommandRegistryBase).inSingletonScope();
@@ -26,4 +27,12 @@ var configuration = Container.get<Configuration>(SERVICE_IDENTIFIERS.CONFIGURATI
 moduleRegistry.loadModules()
     .then(modules => moduleRegistry.preInitializeModules(modules))
     .then(modules => moduleRegistry.initializeModules(client, modules))
-    .then(modules => moduleRegistry.postInitializeModules(client, modules).then(() => client.login(configuration.token)));
+    .then(modules => {
+        client.login(configuration.token);
+        return Promise.resolve(modules);
+    })
+    .then(modules => moduleRegistry.postInitializeModules(client, modules))
+    .catch(reason => console.error(reason))
+    .finally(async () => {
+        console.log("Service Started");
+    });
