612e831ae0b4fb83ee830d67e0a0ecc19a6da664
Bridger Maskrey <maskreybe@outlook.com>
2019-11-08 13:48:33 -0500

    ✨ Bot now responds to !help

--- src/core/core-module.ts
+++ src/core/core-module.ts
@@ -1,24 +1,34 @@
 import { Client } from 'discord.js';
 import i18n = require('i18n');
-import { Module, ModuleInfo, EventHandler } from 'api';
+import { Module, ModuleInfo, EventHandler, Command } from 'api';
 import { CommandHandler, ReadyHandler } from 'core/events';
+import HelpCommand from 'core/commands/help-command';
 
 export default class CoreModule extends Module {
 
     private coreEvents: EventHandler[];
+    private coreCommands: Command[];
 
-    public constructor(moduleInfo: ModuleInfo) {
-        super(moduleInfo);
+    async preInitialize(): Promise<any> {
         this.coreEvents = [
-            new CommandHandler(moduleInfo.id),
-            new ReadyHandler(moduleInfo.id)
+            new CommandHandler(this.moduleInfo.id),
+            new ReadyHandler(this.moduleInfo.id)
         ];
+        this.coreCommands = [
+            new HelpCommand(this.moduleInfo.id)
+        ];
+
+        return Promise.resolve();
     }
 
     async postInitialize(client: Client) : Promise<any> {
         console.info(i18n.__('Loaded core module components'));
 
         return await super.postInitialize(client);
+    }
+
+    get commands(): Command[] {
+        return this.coreCommands;
     }
 
     get events(): EventHandler[] {
