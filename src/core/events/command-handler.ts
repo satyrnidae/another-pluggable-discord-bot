231ae0422d0797ef1d26bdfe06acfa434ec88e86
Bridger Maskrey <maskreybe@outlook.com>
2019-11-08 13:47:24 -0500

    😱 Copied poorly formatted command handler code

--- src/core/events/command-handler.ts
+++ src/core/events/command-handler.ts
@@ -1,6 +1,8 @@
+import i18n = require('i18n');
 import { ParsedMessage, parse as ParseMessage } from 'discord-command-parser';
+import yparser from 'yargs-parser'
 import { Client, Message } from 'discord.js';
-import { CommandRegistry, Configuration, Container, EventHandler, SERVICE_IDENTIFIERS } from 'api';
+import { CommandRegistry, Configuration, Container, EventHandler, SERVICE_IDENTIFIERS, Command } from 'api';
 
 export default class CommandHander extends EventHandler {
     event: string = "message";
@@ -13,15 +15,40 @@ export default class CommandHander extends EventHandler {
         this.commandRegistry = Container.get(SERVICE_IDENTIFIERS.COMMAND_REGISTRY);
     }
 
-    handler(_: Client, message: Message): boolean {
+    //TODO: Refactor this exported code from santa bot
+    handler(client: Client, message: Message): boolean {
         const prefix: string = this.configuration.defaultPrefix; //TODO: Custom prefixes
         const parsedCommand: ParsedMessage = ParseMessage(message, prefix);
 
         if(!parsedCommand.success) return false;
 
-        console.log(`Parsed Command ${parsedCommand.command}`);
+        const commandArgs = parsedCommand.arguments;
+        var commandValue = parsedCommand.command;
+        var commands: Command[] = this.commandRegistry.get(commandValue);
 
-        return true;
+        var senderId: string
+
+        // Gets the ID of the user and the ID of the chat for logging
+        if (message.guild) {
+            senderId = message.member.displayName.concat('@').concat(message.guild.id).concat(':')
+        }
+        else {
+            senderId = message.author.username.concat('@').concat(client.user.id).concat(':')
+        }
+
+        if (commands.length != 1) {
+            console.warn(senderId, i18n.__("Command"), commandValue, i18n.__("did not exist in the list of registered commands (prefix collision?)"))
+            return false;
+        }
+        const command = commands[0]
+
+        var p_argv = yparser(commandArgs, command.options)
+        console.debug(senderId, i18n.__("Execute"), commandValue, commandArgs.join(" "))
+
+        // Check perms and execute
+        if (command.checkPermissions(message)) {
+            command.run(client, message, p_argv)
+        }
     }
 
 
