a66b09a800eb15af8b1b3046d62827a720265b49
Bridger Maskrey <maskreybe@outlook.com>
2019-11-15 17:19:58 -0500

    🦋 forEachAsync is now cancelable

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,12 @@
-export async function forEachAsync(array: any[], callback: (current: any, index: number, array: any[]) => Promise<any>) {
+export class LoopStateArgs {
+    break: boolean;
+}
+
+export async function forEachAsync(array: any[], callback: (current: any, index: number, array: any[], loopStateArgs: LoopStateArgs) => Promise<any>) {
+    const loopStateArgs: LoopStateArgs = new LoopStateArgs();
+
     for(let index: number = 0; index < array.length; ++index) {
-        await callback(array[index], index, array);
+        await callback(array[index], index, array, loopStateArgs);
+        if(loopStateArgs.break) break;
     }
 }
