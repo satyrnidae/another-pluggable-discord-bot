2e98c47c5c756439f945cfa1c1d200b3bdc6b936
Bridger Maskrey <maskreybe@outlook.com>
2019-11-18 16:54:54 -0500

    🐛 Fixed issues with setPrefix and DMs

--- src/core/module/commands/set-prefix-command.ts
+++ src/core/module/commands/set-prefix-command.ts
@@ -1,5 +1,5 @@
 import i18n = require('i18n');
-import { Command } from 'api';
+import { Command, lazyInject, AppConfiguration, SERVICE_IDENTIFIERS } from 'api';
 import { Options, Arguments } from 'yargs-parser';
 import { Message, Client } from 'discord.js';
 import { GuildConfiguration } from 'db';
@@ -19,26 +19,25 @@ export default class SetPrefixCommand extends Command {
         }
     }
 
-    async run(client: Client, message: Message, args: Arguments): Promise<any> {
+    async run(_: Client, message: Message, args: Arguments): Promise<any> {
+        if(!message.guild) {
+            message.reply(i18n.__('I\'m sorry, but I\'m afraid you can\'t change the prefix in a direct message!'));
+        }
 
         const guildConfiguration: GuildConfiguration = await GuildConfiguration.load(message.guild);
-
-        console.log(args);
-
         const prefix: string = args['prefix'] || args._[0];
 
         if(guildConfiguration && prefix) {
             guildConfiguration.commandPrefix = prefix;
-            guildConfiguration.save();
-
+            await guildConfiguration.save();
             return await message.reply(i18n.__('Guild prefix successfully updated to ').concat(`\`${guildConfiguration.commandPrefix}\``)
                 .concat(i18n.__('!')));
         }
-        return await message.reply(i18n.__('Unfortunately, I wasn\'t able to update the prefix with the specified value!').concat('\r\n')
-            .concat('Please feel free to try again!'));
+        return await message.reply(i18n.__('Unfortunately, I wasn\'t able to update the prefix to ').concat(prefix).concat(i18n.__('!'))
+            .concat('\r\n').concat('Please feel free to try again!'));
     }
 
     async checkPermissions(message: Message): Promise<boolean> {
-        return Promise.resolve(message.member.hasPermission('MANAGE_GUILD'));
+        return Promise.resolve(message.member && message.member.hasPermission('MANAGE_GUILD'));
     }
 }
