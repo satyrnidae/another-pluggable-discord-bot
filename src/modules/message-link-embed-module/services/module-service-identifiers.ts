f066d06d41f89264ba59bc8c4020bf49fe647900
Bridger Maskrey <maskreybe@outlook.com>
2019-11-22 12:03:12 -0500

    ✨ Service identifiers for the module

---
+++ src/modules/message-link-embed-module/services/module-service-identifiers.ts
@@ -0,0 +1,7 @@
+const ModuleServiceIdentifiers = {
+    Configuration: Symbol.for('msg-link-embed.Configuration'),
+    Message: Symbol.for('msg-link-embed.Message'),
+    WebRequest: Symbol.for('msg-link-embed.WebRequest')
+};
+
+export default ModuleServiceIdentifiers;
