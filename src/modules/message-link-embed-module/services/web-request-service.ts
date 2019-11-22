eae46353e0220839c180b5ba2a4b785248cb31f4
Bridger Maskrey <maskreybe@outlook.com>
2019-11-22 14:33:22 -0500

    ♻ Use api implementations for xmlhttprequests

--- src/modules/message-link-embed-module/services/web-request-service.ts
+++ src/modules/message-link-embed-module/services/web-request-service.ts
@@ -1,30 +1,16 @@
-import { XMLHttpRequest } from 'xmlhttprequest';
 import { injectable } from 'inversify';
+import { getHttpResponse } from 'api';
 
-type Resolve = (value?: string | PromiseLike<string>) => void;
-type Reject = (reason?: any) => void;
 const TIMEOUT: number = 500;
 const OK: number = 200;
 
 @injectable()
 export default class WebRequestService {
     async getContentType(url: string, timeout: number = TIMEOUT): Promise<string> {
-        return new Promise<string>((resolve: Resolve, reject: Reject): void => {
-            const xhr: XMLHttpRequest = new XMLHttpRequest();
-            xhr.timeout = timeout;
-            xhr.open('HEAD', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState === xhr.DONE) {
-                    if(xhr.status === OK) {
-                        resolve(xhr.getResponseHeader('Content-Type'));
-                    }
-                    else {
-                        reject(xhr.statusText);
-                    }
-                }
-            }
-            xhr.onerror = () => reject(xhr.statusText);
-            xhr.send();
-        });
+        const httpResponse = await getHttpResponse('HEAD', url, timeout);
+        if(httpResponse.status != OK) {
+            return Promise.reject(httpResponse.statusText);
+        }
+        return httpResponse.getResponseHeader('Content-Type');
     }
 }
