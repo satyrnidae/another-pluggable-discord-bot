7ff6a23bb869220b24a6c03607b16e990e96fa97
Bridger Maskrey <maskreybe@outlook.com>
2019-11-22 12:03:52 -0500

    🏷 Exports for services

---
+++ src/modules/message-link-embed-module/services/index.ts
@@ -0,0 +1,4 @@
+export { default as ModuleServiceIdentifiers } from 'modules/message-link-embed-module/services/module-service-identifiers';
+export { default as ModuleConfigurationService } from 'modules/message-link-embed-module/services/module-configuration-service';
+export { default as WebRequestService } from 'modules/message-link-embed-module/services/web-request-service';
+export { default as MessageService } from 'modules/message-link-embed-module/services/message-service';
