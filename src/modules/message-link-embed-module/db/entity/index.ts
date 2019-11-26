f3a3c2451436c69c6fe7b8eb2cd15655f6bf13fb
Isabel Maskrey <isabel@satyrn.dev>
2019-11-26 10:38:12 -0500

    ✨ Add message link history

--- src/modules/message-link-embed-module/db/entity/index.ts
+++ src/modules/message-link-embed-module/db/entity/index.ts
@@ -1,4 +1,5 @@
+export * from 'modules/message-link-embed-module/db/entity/message-link-history';
 export * from 'modules/message-link-embed-module/db/entity/message-history';
-export * from 'modules/message-link-embed-module/db/entity/user-history';
+export * from 'modules/message-link-embed-module/db/entity/user-settings';
 export * from 'modules/message-link-embed-module/db/entity/channel-history';
 export * from 'modules/message-link-embed-module/db/entity/guild-history';
---
+++ src/modules/message-link-embed-module/db/entity/message-link-history.ts
@@ -0,0 +1,36 @@
+import { DataEntity } from 'api/db';
+import { PrimaryGeneratedColumn, ManyToOne, ManyToMany, DeleteResult, Entity, JoinTable } from 'typeorm';
+import { MessageHistory } from 'modules/message-link-embed-module/db/entity';
+import { lazyInject } from 'api/inversion';
+import { DataService } from 'core/services';
+import { ServiceIdentifiers } from 'api/services';
+
+@Entity('msg_link_embed_message_link_history')
+export class MessageLinkHistory extends DataEntity {
+
+    @lazyInject(ServiceIdentifiers.Data)
+    private readonly dataService!: DataService;
+
+    async save(): Promise<this & MessageLinkHistory> {
+        const repository = await this.dataService.getRepository(MessageLinkHistory);
+        return repository.save(this);
+    }
+
+    async delete(): Promise<DeleteResult> {
+        const repository = await this.dataService.getRepository(MessageLinkHistory);
+        return repository.delete(this);
+    }
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => MessageHistory)
+    requestMessage: MessageHistory;
+
+    @ManyToOne(() => MessageHistory)
+    originMessage: MessageHistory;
+
+    @ManyToMany(() => MessageHistory)
+    @JoinTable({name: 'msg_link_embed_join_messages_to_link_history'})
+    resultMessages: MessageHistory[];
+}
