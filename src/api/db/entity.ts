c68403f4461f0793d090ddd5a4c7e15cfcf01693
Isabel Maskrey <isabel@satyrn.dev>
2019-11-26 10:34:22 -0500

    🗃 Added audit fields to data entity

--- src/api/db/entity.ts
+++ src/api/db/entity.ts
@@ -1,3 +1,11 @@
+import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
 export abstract class DataEntity {
     abstract save(): Promise<this & DataEntity>;
+
+    @CreateDateColumn()
+    createDate: Date;
+
+    @UpdateDateColumn()
+    updateDate: Date;
 }
