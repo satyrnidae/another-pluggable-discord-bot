5b84c2d19918964687de04d625c96223300e56d3
Isabel Maskrey <isabel@satyrn.dev>
2019-12-18 10:17:31 -0500

    🚚 Moved typings file

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -5,3 +5,11 @@ type RepositoryTarget<T> = string | Function | (new () => T) | import('typeorm')
 type Resolve<T> = (value?: T | PromiseLike<T>) => void;
 type Reject = (reason?: any) => void;
 type UnionArray<T> = T | T[];
+type ReadFileOptions = {
+    encoding?: string;
+    flag?: string
+};
+type ReadDirOptions = {
+    encoding: BufferEncoding;
+    withFileTypes?: false;
+} | 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'latin1' | 'binary' | 'hex';
