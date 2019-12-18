26b333ada140801d342df73beb74834c798662b4
Isabel Maskrey <isabel@satyrn.dev>
2019-12-18 11:38:53 -0500

    ♻ Refactored app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
-import * as sapi from 'api/services';
-import { Container } from 'api/inversion';
-import { Lifecycle } from 'api/lifecycle';
-import Robot from 'core/robot';
-import { CommandService, ConfigurationService, ModuleService, DataService, ClientService, EventService } from 'core/services';
+import { Container } from '/src/api/inversion';
+import { Lifecycle } from '/src/api/lifecycle';
+import {
+    CommandService as ICommandService, ConfigurationService as IConfigurationService, ModuleService as IModuleService, DataService as IDataService,
+    ClientService as IClientService, EventService as IEventService, ServiceIdentifiers
+} from '/src/api/services';
+import { GuildConfigurationFactory } from '/src/db/factory';
+import { CommandService, ConfigurationService, DataService, ClientService, EventService, ModuleService } from '/src/core/services';
+import { Robot } from '/src/core/robot';
 
 // Set Up Dependencies
-Container.bind<sapi.CommandService>(sapi.ServiceIdentifiers.Command).to(CommandService).inSingletonScope();
-Container.bind<sapi.ConfigurationService>(sapi.ServiceIdentifiers.Configuration).to(ConfigurationService).inSingletonScope();
-Container.bind<sapi.ModuleService>(sapi.ServiceIdentifiers.Module).to(ModuleService).inSingletonScope();
-Container.bind<sapi.DataService>(sapi.ServiceIdentifiers.Data).to(DataService).inSingletonScope();
-Container.bind<sapi.ClientService>(sapi.ServiceIdentifiers.Client).to(ClientService).inSingletonScope();
-Container.bind<sapi.EventService>(sapi.ServiceIdentifiers.Event).to(EventService).inSingletonScope();
+Container.bind<ICommandService>(ServiceIdentifiers.Command).to(CommandService).inSingletonScope();
+Container.bind<IConfigurationService>(ServiceIdentifiers.Configuration).to(ConfigurationService).inSingletonScope();
+Container.bind<IModuleService>(ServiceIdentifiers.Module).to(ModuleService).inSingletonScope();
+Container.bind<IDataService>(ServiceIdentifiers.Data).to(DataService).inSingletonScope();
+Container.bind<IClientService>(ServiceIdentifiers.Client).to(ClientService).inSingletonScope();
+Container.bind<IEventService>(ServiceIdentifiers.Event).to(EventService).inSingletonScope();
+Container.bind<GuildConfigurationFactory>(GuildConfigurationFactory).toSelf();
 Container.bind<Lifecycle>(Robot).toSelf().inSingletonScope();
 
 const robot: Lifecycle = Container.resolve(Robot);
-
 robot.preInitialize()
     .then(() => robot.initialize())
     .then(() => robot.postInitialize())
