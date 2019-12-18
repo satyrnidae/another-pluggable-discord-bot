b6d0b9eaefe89f9a755f71e4a5d749ed958622cb
Isabel Maskrey <isabel@satyrn.dev>
2019-12-18 10:30:50 -0500

    💡 Documented /src/api/inversion

--- src/api/inversion.ts
+++ src/api/inversion.ts
@@ -2,7 +2,15 @@ import 'reflect-metadata';
 import * as Inversify from 'inversify';
 import getDecorators from 'inversify-inject-decorators';
 
+/**
+ * The IOC container instance
+ */
 const Container = new Inversify.Container();
-const { lazyInject, lazyInjectNamed, lazyInjectTagged, lazyMultiInject } = getDecorators(Container, false);
+const injectors = getDecorators(Container, false);
 
-export { Container, lazyInject, lazyInjectNamed, lazyInjectTagged, lazyMultiInject };
+/**
+ * Injects an instance from the container into a property on first access
+ */
+const lazyInject = injectors.lazyInject;
+
+export { Container, lazyInject };
