8cec09e6f78b5ead9a490b5a4409e8709ce8b93b
Isabel Maskrey <isabel@satyrn.dev>
2019-12-18 00:42:22 -0500

    🏷 Updated index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
 declare module 'xmlhttprequest';
 
+type EventHandlerFunction = (...args: any[]) => void;
+type RepositoryTarget<T> = string | Function | (new () => T) | import('typeorm').EntitySchema<T>;
 type Resolve<T> = (value?: T | PromiseLike<T>) => void;
 type Reject = (reason?: any) => void;
 type UnionArray<T> = T | T[];
