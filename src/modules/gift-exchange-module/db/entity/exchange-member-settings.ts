e005c7f84db3caa1cb54f44201a2d1256ddf2e60
Isabel Maskrey <isabel@satyrn.dev>
2019-12-17 22:12:15 -0500

    💩 Rollback half-baked bullcrap

--- src/modules/gift-exchange-module/db/entity/exchange-member-settings.ts
+++ src/modules/gift-exchange-module/db/entity/exchange-member-settings.ts
@@ -1,5 +1,5 @@
 import { DataEntity } from 'api/db';
-import { Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne, JoinColumn, Column } from 'typeorm';
+import { Entity, ManyToOne, JoinColumn, Column } from 'typeorm';
 import { Exchange } from 'modules/gift-exchange-module/db/entity/exchange';
 import { ExchangeMember } from 'modules/gift-exchange-module/db/entity/exchange-member';
 
--- src/modules/gift-exchange-module/db/entity/index.ts
+++ src/modules/gift-exchange-module/db/entity/index.ts
@@ -1,4 +1,4 @@
-export * from 'modules/gift-exchange-module/db/entity/exchange-preferences';
 export * from 'modules/gift-exchange-module/db/entity/exchange-member';
 export * from 'modules/gift-exchange-module/db/entity/exchange';
+export * from 'modules/gift-exchange-module/db/entity/exchange-member-settings';
 export * from 'modules/gift-exchange-module/db/entity/guild-instance';
