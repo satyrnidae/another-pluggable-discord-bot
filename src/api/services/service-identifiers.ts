76537180e54959091b331faf06e0835b790465d6
Isabel Maskrey <isabel@satyrn.dev>
2019-12-18 09:52:17 -0500

    📝 Docs for service identifiers

--- src/api/services/service-identifiers.ts
+++ src/api/services/service-identifiers.ts
@@ -2,11 +2,34 @@
  * Symbols for all of the services which are available from the IOC container.
  */
 const ServiceIdentifiers = {
+    /**
+     * Symbol for the configuration service
+     */
     Configuration: Symbol.for('/src/api/services/ConfigurationService'),
+
+    /**
+     * Symbol for the client service
+     */
     Client: Symbol.for('/src/api/services/ClientService'),
+
+    /**
+     * Symbol for the command service
+     */
     Command: Symbol.for('/src/api/services/CommandService'),
+
+    /**
+     * Symbol for the data service
+     */
     Data: Symbol.for('/src/api/services/DataService'),
+
+    /**
+     * Symbol for the event service
+     */
     Event: Symbol.for('/src/api/services/EventService'),
+
+    /**
+     * Symbol for the module service
+     */
     Module: Symbol.for('/src/api/services/ModuleService')
 };
 
